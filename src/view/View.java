2b9ebb6b60d815754a6a313b17df161e6c0650df
sieb <devnull@localhost>
2014-09-11 23:58:38 +0200

    a

--- src/view/View.java
+++ src/view/View.java
@@ -1,14 +1,38 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class View extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private final int initWidth = 288;
+	private final int initHeight = 512;
 
 	public View() {
 		
+		super();
+		init();
 		
+	}
+	
+	public void init() {
+		
+		setSize(initWidth, initHeight);
+		setResizable(false);
+		setContentPane(getPanel());
+		
+	}
+	
+	public JPanel getPanel() {
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		
+		
+		
+		return panel;
 		
 	}
 
